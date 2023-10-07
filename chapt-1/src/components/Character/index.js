33833c6ecbbc1f01d24b2a49d125765694b54146
Aaron Dantley <aadantley@gmail.com>
2023-10-07 17:07:36 -0400

    pushing chapt-1 project
    Rick and morty API v1

---
+++ chapt-1/src/components/Character/index.js
@@ -0,0 +1,19 @@
+export default function Character(character) {
+  return (
+    <div className="col-3">
+      <div className="card">
+        <img
+          src={character.image}
+          alt={character.name}
+          width="300"
+          className="card-img-top"
+        />
+
+        <div className="card-body">
+          <h3 className="card-title">{character.name}</h3>
+          <p>Origin: {character.origin && character.origin.name}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ chapt-1/src/components/Header/index.js
@@ -0,0 +1,10 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <p>Edit <code>src/App.js</code> and save to reload.</p>
+        </header>
+    )
+}
